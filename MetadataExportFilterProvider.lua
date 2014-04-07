ad1099298ed7e97a8e1b75dda64aaa8bcaadc741
Morgan Prior <morgan@amaras-tech.co.uk>
2014-04-07 22:13:47 +0100

    Retab Metadata*.lua

--- MetadataExportFilterProvider.lua
+++ MetadataExportFilterProvider.lua
@@ -20,7 +20,7 @@ of it requires the prior written permission of Adobe.
 ------------------------------------------------------------------------------]]
 
 local LrView = import 'LrView'
-local bind = LrView.bind
+local bind   = LrView.bind
 
 --------------------------------------------------------------------------------
 -- This function will check the status of the Export Dialog to determine 
@@ -28,29 +28,29 @@ local bind = LrView.bind
 
 local function updateFilterStatus( propertyTable, ... )
 
-	-- Initialise potential error message.
-	local message = nil
-	
-	if propertyTable.metachoice == nil then
-		message = LOC "$$$/SDK/MetaExportFilter/Messages/choice=Please choose which type of metadata to filter"
+  -- Initialise potential error message.
+  local message = nil
+  
+  if propertyTable.metachoice == nil then
+    message = LOC "$$$/SDK/MetaExportFilter/Messages/choice=Please choose which type of metadata to filter"
         elseif propertyTable.metavalue == nil then
-		message = LOC "$$$/SDK/MetaExportFilter/Messages/choice=Please enter the required matching string"
-	end
-	
-	if message then
-		-- Display error.
-		propertyTable.message      = message
-		propertyTable.hasError     = true
-		propertyTable.hasNoError   = false
-		propertyTable.LR_canExport = false
+    message = LOC "$$$/SDK/MetaExportFilter/Messages/choice=Please enter the required matching string"
+  end
+  
+  if message then
+    -- Display error.
+    propertyTable.message      = message
+    propertyTable.hasError     = true
+    propertyTable.hasNoError   = false
+    propertyTable.LR_canExport = false
 
-	else
-		-- All required fields have been populated so enable Export button, reset message, and set error status to false.
-		propertyTable.message      = nil
-		propertyTable.hasError     = false
-		propertyTable.hasNoError   = true
-		propertyTable.LR_canExport = true
-	end
+  else
+    -- All required fields have been populated so enable Export button, reset message, and set error status to false.
+    propertyTable.message      = nil
+    propertyTable.hasError     = false
+    propertyTable.hasNoError   = true
+    propertyTable.LR_canExport = true
+  end
 end
 
 --------------------------------------------------------------------------------
@@ -59,9 +59,9 @@ end
 
 local function startDialog( propertyTable )
 
-	propertyTable:addObserver( 'metachoice', updateFilterStatus )
-	propertyTable:addObserver( 'metavalue',  updateFilterStatus )
-	updateFilterStatus( propertyTable )
+  propertyTable:addObserver( 'metachoice', updateFilterStatus )
+  propertyTable:addObserver( 'metavalue',  updateFilterStatus )
+  updateFilterStatus( propertyTable )
 
 end
 
@@ -70,40 +70,40 @@ end
 -- when this filter is added to the export session.
 
 local function sectionForFilterInDialog( f, propertyTable )
-	
-	return {
-		title = LOC "$$$/SDK/MetaExportFilter/SectionTitle=Metadata Filter",
-		f:row {
-			spacing = f:control_spacing(),
-			f:static_text {
-				title = "Description of this field",
-			},
+  
+  return {
+    title = LOC "$$$/SDK/MetaExportFilter/SectionTitle=Metadata Filter",
+    f:row {
+      spacing = f:control_spacing(),
+      f:static_text {
+        title = "Description of this field",
+      },
 
-			f:popup_menu {
-				value = bind 'metachoice',
-				items = {
-					{ title = "Title",			      value = "title" },
-					{ title = "Creator",			    value = "creator" },
-					{ title = "Copyright Status",	value = "copyright" },
-					{ title = "File Name",			  value = "fileName" },
-					{ title = "Folder",	          value = "folderName" },
-				},
-			},
+      f:popup_menu {
+        value = bind 'metachoice',
+        items = {
+          { title = "Title",            value = "title" },
+          { title = "Creator",          value = "creator" },
+          { title = "Copyright Status", value = "copyright" },
+          { title = "File Name",        value = "fileName" },
+          { title = "Folder",           value = "folderName" },
+        },
+      },
 
-			f:edit_field {
-				value = bind 'metavalue',
-				fill_horizontal = 1,
-			},
-		}
-	}
+      f:edit_field {
+        value = bind 'metavalue',
+        fill_horizontal = 1,
+      },
+    }
+  }
 end
 
 --------------------------------------------------------------------------------
 -- We specify any presets here
 
 local exportPresetFields = {
-	{ key = 'metachoice', 	default = "Creator" },
-	{ key = 'metavalue', 	  default = 'default' },
+  { key = 'metachoice',   default = "Creator" },
+  { key = 'metavalue',    default = 'default' },
 }
 
 --------------------------------------------------------------------------------
@@ -111,31 +111,31 @@ local exportPresetFields = {
 
 local function shouldRenderPhoto( exportSettings, photo )
 
-	-- This function should return either: 
-	--   true, photo gets included in export, or
-	--   false, photo should be removed from the export.
-	-- The photo represents an LrPhoto object.
-	
-	local targetField = exportSettings.metachoice
-	local targetValue = exportSettings.metavalue
+  -- This function should return either: 
+  --   true, photo gets included in export, or
+  --   false, photo should be removed from the export.
+  -- The photo represents an LrPhoto object.
+  
+  local targetField = exportSettings.metachoice
+  local targetValue = exportSettings.metavalue
 
-	-- Now that the user has filled in this section, we need to filter through the images and only 
-	-- choose the ones that match the required metadata.
+  -- Now that the user has filled in this section, we need to filter through the images and only 
+  -- choose the ones that match the required metadata.
 
-	local sourceMetaValue = photo:getFormattedMetadata( targetField )
-	
+  local sourceMetaValue = photo:getFormattedMetadata( targetField )
+  
   local  shouldRender   = sourceMetaValue == targetValue
 
-	return shouldRender
-	
+  return shouldRender
+  
 end
 
 --------------------------------------------------------------------------------
 
 return {
 
-	exportPresetFields       = exportPresetFields,
-	startDialog              = startDialog,
-	sectionForFilterInDialog = sectionForFilterInDialog,
-	shouldRenderPhoto        = shouldRenderPhoto,
+  exportPresetFields       = exportPresetFields,
+  startDialog              = startDialog,
+  sectionForFilterInDialog = sectionForFilterInDialog,
+  shouldRenderPhoto        = shouldRenderPhoto,
 }
